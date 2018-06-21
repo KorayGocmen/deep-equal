defe643735afff751d7361a9c68d6600e6d228d1
Koray Gocmen <gocmen.koray@gmail.com>
2018-06-21 12:25:13 -0400

    more tests

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ var isDeepEqual = (param1, param2) => {
   if (param1.constructor === Array) {
     if (param1.length !== param2.length) return false;
     for (var i = 0; i < param1.length; i++) {
-      if (param1[i] !== param2[i]) return false;
+      if (param1[i].constructor === Array || param1[i].constructor === Object) {
+        return isDeepEqual(param1[i], param2[i]);
+      } else if (param1[i] !== param2[i]) return false;
     }
   } else if (param1.constructor === Object) {
     if (Object.keys(param1).length !== Object.keys(param2).length) return false;
     for (var i = 0; i < Object.keys(param1).length; i++) {
       var key = Object.keys(param1)[i];
-      if (param1[key] !== param2[key]) return false;
+      if (
+        param1[key].constructor === Array ||
+        param1[key].constructor === Object
+      ) {
+        return isDeepEqual(param1[key], param2[key]);
+      } else if (param1[key] !== param2[key]) return false;
     }
   } else if (param1.constructor == String || param1.constructor == Number) {
     return param1 === param2;
--- test.js
+++ test.js
@@ -1,125 +1,153 @@
 "use strict";
 
 require("mocha");
-const assert = require("assert");
+var assert = require("assert");
 
-const isDeepEqual = require("./index");
+var isDeepEqual = require("./index");
 
-describe("deep equal", () => {
-  describe("testing arrays", () => {
-    it("should return true when arrays are equal", function () {
-      const param1 = [1, 2, 3, 4];
-      const param2 = [1, 2, 3, 4];
-      const expectedResult = true;
+describe("deep equal", function() {
+  describe("testing arrays", function() {
+    it("should return true when arrays are equal", function() {
+      var param1 = [1, 2, 3, 4];
+      var param2 = [1, 2, 3, 4];
+      var expectedResult = true;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return true when arrays are both empty", function () {
-      const param1 = [];
-      const param2 = [];
-      const expectedResult = true;
+    it("should return true when arrays are both empty", function() {
+      var param1 = [];
+      var param2 = [];
+      var expectedResult = true;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return false when arrays are not equal in size", function () {
-      const param1 = [1, 2, 3, 4, 5];
-      const param2 = [1, 2, 3, 4];
-      const expectedResult = false;
+    it("should return true when arrays have objects and are equal", function() {
+      var param1 = [{ key: "val1", key2: "test" }, { key: "val2" }];
+      var param2 = [{ key: "val1", key2: "test" }, { key: "val2" }];
+      var expectedResult = true;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return false when arrays are equal in size both different in type", function () {
-      const param1 = [1, 2, 3, 4, 5];
-      const param2 = [1, 2, 3, 4, "5"];
-      const expectedResult = false;
+    it("should return false when arrays have objects and are not equal", function() {
+      var param1 = [{ key: "val1" }, { key: "val" }];
+      var param2 = [{ key: "val1" }, { key: "DIFFERENT" }];
+      var expectedResult = false;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return false when comparing arrays and objects", function () {
-      const param1 = [1, 2, 3, 4, 5];
-      const param2 = { key: "val" };
-      const expectedResult = false;
+    it("should return false when arrays are not equal in size", function() {
+      var param1 = [1, 2, 3, 4, 5];
+      var param2 = [1, 2, 3, 4];
+      var expectedResult = false;
+      assert.equal(isDeepEqual(param1, param2), expectedResult);
+    });
+
+    it("should return false when arrays are equal in size both different in type", function() {
+      var param1 = [1, 2, 3, 4, 5];
+      var param2 = [1, 2, 3, 4, "5"];
+      var expectedResult = false;
+      assert.equal(isDeepEqual(param1, param2), expectedResult);
+    });
+
+    it("should return false when comparing arrays and objects", function() {
+      var param1 = [1, 2, 3, 4, 5];
+      var param2 = { key: "val" };
+      var expectedResult = false;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
   });
 
   describe("testing objects", () => {
-    it("should return true when object are equal", function () {
-      const param1 = { key1: "val1", key2: "val2" };
-      const param2 = { key1: "val1", key2: "val2" };
-      const expectedResult = true;
+    it("should return true when object are equal", function() {
+      var param1 = { key1: "val1", key2: "val2" };
+      var param2 = { key1: "val1", key2: "val2" };
+      var expectedResult = true;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return true when objects are both empty", function () {
-      const param1 = {};
-      const param2 = {};
-      const expectedResult = true;
+    it("should return true when objects are both empty", function() {
+      var param1 = {};
+      var param2 = {};
+      var expectedResult = true;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return false when objects are not equal in size", function () {
-      const param1 = { key1: "val1", key2: "val2", key3: "val3" };
-      const param2 = { key1: "val1", key2: "val2" };
-      const expectedResult = false;
+    it("should return true when objects have arrays as values and equal", function() {
+      var param1 = { arr: [1, 2, 3] };
+      var param2 = { arr: [1, 2, 3] };
+      var expectedResult = true;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return false when arrays are equal in size both different in type", function () {
-      const param1 = { key1: 1, key2: "val2" };
-      const param2 = { key1: "1", key2: "val2" };
-      const expectedResult = false;
+    it("should return false when objects have arrays as values and not equal", function() {
+      var param1 = { arr: [1, 2, 3] };
+      var param2 = { arr: [1, 2, 3, 4] };
+      var expectedResult = false;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return false when comparing objects and arrays", function () {
-      const param1 = [1, 2, 3, 4, 5];
-      const param2 = { key: "val" };
-      const expectedResult = false;
+    it("should return false when objects are not equal in size", function() {
+      var param1 = { key1: "val1", key2: "val2", key3: "val3" };
+      var param2 = { key1: "val1", key2: "val2" };
+      var expectedResult = false;
+      assert.equal(isDeepEqual(param1, param2), expectedResult);
+    });
+
+    it("should return false when arrays are equal in size both different in type", function() {
+      var param1 = { key1: 1, key2: "val2" };
+      var param2 = { key1: "1", key2: "val2" };
+      var expectedResult = false;
+      assert.equal(isDeepEqual(param1, param2), expectedResult);
+    });
+
+    it("should return false when comparing objects and arrays", function() {
+      var param1 = [1, 2, 3, 4, 5];
+      var param2 = { key: "val" };
+      var expectedResult = false;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
   });
 
   describe("testing numbers and strings", () => {
-    it("should return true when strings are equal", function () {
-      const param1 = "string1";
-      const param2 = "string1";
-      const expectedResult = true;
+    it("should return true when strings are equal", function() {
+      var param1 = "string1";
+      var param2 = "string1";
+      var expectedResult = true;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return true when integers are equal", function () {
-      const param1 = 1999;
-      const param2 = 1999;
-      const expectedResult = true;
+    it("should return true when integers are equal", function() {
+      var param1 = 1999;
+      var param2 = 1999;
+      var expectedResult = true;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return true when floats are equal", function () {
-      const param1 = 1999.9999;
-      const param2 = 1999.9999;
-      const expectedResult = true;
+    it("should return true when floats are equal", function() {
+      var param1 = 1999.9999;
+      var param2 = 1999.9999;
+      var expectedResult = true;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return false when strings are not equal", function () {
-      const param1 = "string12";
-      const param2 = "string1";
-      const expectedResult = false;
+    it("should return false when strings are not equal", function() {
+      var param1 = "string12";
+      var param2 = "string1";
+      var expectedResult = false;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return true when integers are not equal", function () {
-      const param1 = 19999;
-      const param2 = 1999;
-      const expectedResult = false;
+    it("should return true when integers are not equal", function() {
+      var param1 = 19999;
+      var param2 = 1999;
+      var expectedResult = false;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
 
-    it("should return true when floats are not equal", function () {
-      const param1 = 1999.9999;
-      const param2 = 1999.99;
-      const expectedResult = false;
+    it("should return true when floats are not equal", function() {
+      var param1 = 1999.9999;
+      var param2 = 1999.99;
+      var expectedResult = false;
       assert.equal(isDeepEqual(param1, param2), expectedResult);
     });
   });
